7f26055c06b153e48a988a133104179c607d5338
mindwind <mindwind@163.com>
2014-12-31 13:53:58 +0800

    new - TransferArmorFilter

---
+++ src/main/java/io/craft/armor/TransferArmorFilter.java
@@ -0,0 +1,26 @@
+package io.craft.armor;
+
+import io.craft.armor.spi.ArmorInvocation;
+
+/**
+ * Transfer armor filter.
+ * Transfer delegate object to link object.
+ * 
+ * @author mindwind
+ * @version 1.0, Dec 31, 2014
+ */
+public class TransferArmorFilter extends AbstractArmorFilter {
+	
+
+	@Override
+	public void doFilter(ArmorInvocation invocation) throws Throwable {
+		if (isOff(this.getClass())) { return; }
+		
+		Object delegateObject = invocation.getDelegateObject();
+		Object transferObject = context.getTransferObject(delegateObject);
+		if (transferObject == null) { return; }
+		
+		invocation.setDelegateObject(transferObject);
+	}
+
+}
